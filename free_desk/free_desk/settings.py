01e53e9785a05a4bdfbcf7242e72e92f571ada2b
alexey mogelat <alexey_mogelat@live.com>
2020-04-05 01:20:14 +0300

    updated settings

--- free_desk/free_desk/settings.py
+++ free_desk/free_desk/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'desk.apps.DeskConfig'
+    'desk.apps.DeskConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
